f36e5fb1a2f62dadfc32a8da186976080563380f
David Lukes <dafydd.lukes@gmail.com>
2015-04-17 11:29:32 +0200

    report multiple verification errors per check

--- validate.py
+++ validate.py
@@ -11,17 +11,26 @@ import pdb
 from lxml import etree
 
 class VerificationError(Exception):
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, error_log):
+        self.error_log = error_log
 
     def __str__(self):
-        return repr(self.value)
+        return "  " + "\n  ".join(self.error_log)
 
 def verify(tree):
-    vocab_meta(tree)
-    vocab_META(tree)
-    tier_attribs(tree)
-    hierarchy(tree)
+    """Run all post-validation verification checks.
+
+    """
+    errors = []
+
+    for check in (vocab_meta, vocab_META, tier_attribs, hierarchy):
+        try:
+            check(tree)
+        except VerificationError as e:
+            errors += e.error_log
+
+    if errors:
+        raise VerificationError(errors)
 
 def vocab_meta(tree):
     """Verify that meta tiers only have entries allowed by the constrained
@@ -62,10 +71,14 @@ def vocab_meta(tree):
         "zakoktání",
         "zívání"
     ])
+    errors = []
 
     for meta in tree.xpath("//TIER[@LINGUISTIC_TYPE_REF = 'meta']//ANNOTATION_VALUE/text()"):
         if meta not in vocab:
-            raise VerificationError("'{}' is not allowed in a meta tier.".format(meta))
+            errors.append("'{}' is not allowed in a meta tier.".format(meta))
+
+    if errors:
+        raise VerificationError(errors)
 
 def vocab_META(tree):
     """Verify that META tier only has entries allowed by the constrained
@@ -97,33 +110,45 @@ def vocab_META(tree):
         "zvuk z televize",
         "zvuky při jídle"
     ])
+    errors = []
 
     for meta in tree.xpath("//TIER[@LINGUISTIC_TYPE_REF = 'META']//ANNOTATION_VALUE/text()"):
         if meta not in vocab:
-            raise VerificationError("'{}' is not allowed in a META tier.".format(meta))
+            errors.append("'{}' is not allowed in a META tier.".format(meta))
+
+    if errors:
+        raise VerificationError(errors)
 
 def tier_attribs(tree):
     """Verify that the tier attributes are consistent and conform to
     requirements.
 
     """
+    errors = []
+
     if tree.xpath("//TIER[@LINGUISTIC_TYPE_REF = 'fonetický' and not(@PARENT_REF)]"):
-        raise VerificationError("TIERs with @LINGUISTIC_TYPE_REF = 'fonetický' should have a @PARENT_REF attribute.")
+        errors.append("TIERs with @LINGUISTIC_TYPE_REF = 'fonetický' should have a @PARENT_REF attribute.")
 
     for fon_tier in tree.xpath("//TIER[@LINGUISTIC_TYPE_REF = 'fonetický']"):
-        # pdb.set_trace()
         tier_id_prefix = fon_tier.attrib["TIER_ID"].split(" ")[0]
         parent_ref_prefix = fon_tier.attrib["PARENT_REF"].split(" ")[0]
         if not tier_id_prefix == parent_ref_prefix:
-            raise VerificationError("The numeric prefixes of @TIER_ID and @PARENT_REF on a TIER with @LINGUISTIC_TYPE_REF = 'fonetický' should match.")
+            errors.append("The numeric prefixes of @TIER_ID and @PARENT_REF on a TIER with @LINGUISTIC_TYPE_REF = 'fonetický' should match.")
+
+    if errors:
+        raise VerificationError(errors)
 
 def hierarchy(tree):
     """Verify that some forbidden nesting of elements does not occur.
 
     """
+    errors = []
 
     if tree.xpath("//TIER[@PARENT_REF]//ALIGNABLE_ANNOTATION"):
-        raise VerificationError("ALIGNABLE_ANNOTATIONs are not allowed on a TIER with a @PARENT_REF attribute.")
+        errors.append("ALIGNABLE_ANNOTATIONs are not allowed on a TIER with a @PARENT_REF attribute.")
+
+    if errors:
+        raise VerificationError(errors)
 
 schema_root = etree.parse("EAFv2.7_UCNK.xsd").getroot()
 schema = etree.XMLSchema(schema_root)
@@ -143,12 +168,13 @@ for f in sys.argv[1:]:
         ERRORS = True
         sys.stderr.write("Validation error(s) in {}:\n  ".format(f))
         sys.stderr.write("\n  ".join(map(str, validation_errors.error_log)))
+        sys.stderr.write("\n")
 
     try:
         verify(tree)
     except VerificationError as e:
         ERRORS = True
-        sys.stderr.write("Verification error in {}: {}\n".format(f, str(e)))
+        sys.stderr.write("Verification error(s) in {}:\n{}\n".format(f, str(e)))
 
 if ERRORS:
     sys.stderr.write("""
